a15ee13a86f708f7bc26cafc95a9638000ae37be
NaPiZip <napizip@mail.com>
2018-12-10 21:24:37 -0500

    Initial project setup.

--- Bluetooth_sniffer_tool/mainwindow.cpp
+++ Bluetooth_sniffer_tool/mainwindow.cpp
@@ -1,14 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+#include <QtGlobal>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QStringList listOfStrings {"Was", "Geht"};
+
+    ui->cbBluetoothDevices->addItems(listOfStrings);
+    connect(ui->cbBluetoothDevices, QOverload<int>::of(&QComboBox::activated), this, [](){qDebug() << "Pressed";});
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
