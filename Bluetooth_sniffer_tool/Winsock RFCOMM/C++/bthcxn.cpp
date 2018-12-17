664dced22a91905984e8a53e663aa9951769b2a6
NaPiZip <napizip@mail.com>
2018-12-16 19:05:30 -0500

    file for testing API calls.

--- Bluetooth_sniffer_tool/Winsock RFCOMM/C++/bthcxn.cpp
+++ Bluetooth_sniffer_tool/Winsock RFCOMM/C++/bthcxn.cpp
@@ -24,7 +24,9 @@
 #include <ws2bth.h>
 #include <strsafe.h>
 #include <intsafe.h>
+#include <bluetoothapis.h>
 
+#include "dev\testenv.h"
 
 // {B62C4E8D-62CC-404b-BBBF-BF3E3BBB1374}
 DEFINE_GUID(g_guidServiceClass, 0xb62c4e8d, 0x62cc, 0x404b, 0xbb, 0xbf, 0xbf, 0x3e, 0x3b, 0xbb, 0x13, 0x74);
@@ -53,9 +55,6 @@ ULONG RunClientMode(_In_ SOCKADDR_BTH ululRemoteBthAddr, _In_ int iMaxCxnCycles 
 ULONG RunServerMode(_In_ int iMaxCxnCycles = 1);
 void  ShowCmdLineHelp(void);
 ULONG ParseCmdLine(_In_ int argc, _In_reads_(argc) wchar_t * argv[]);
-
-void TestFunction(void);
-BOOL FindingBtDevices();
 
 
 
@@ -135,60 +134,14 @@ int _cdecl wmain(_In_ int argc, _In_reads_(argc)wchar_t *argv[])
             //
 
             //ulRetCode = RunServerMode(g_ulMaxCxnCycles);
-
-			//FindingBtDevices();
-			TestFunction();
+			WSALookupAvailableDevices();
+			DiscoverRadio();
+			DiscoverDevices();			
 			WSACleanup();
         }
     }
 
     return(int)ulRetCode;
-}
-void TestFunction(void)
-{
-	WSAQUERYSET wsaq{0};
-	HANDLE hLookup;
-	LPWSAQUERYSET pwsaResults;	
-	char buffer[4096];
-	DWORD swSize = sizeof(buffer);
-	int nDevicesFound = 1;
-	CSADDR_INFO addrInfo;
-	SOCKADDR_BTH* btSock;
-
-	wsaq.dwNameSpace = NS_BTH;
-	wsaq.dwSize = sizeof(WSAQUERYSET);
-	wsaq.lpcsaBuffer = NULL;
-
-	if (WSALookupServiceBegin(&wsaq, LUP_CONTAINERS, &hLookup) == SOCKET_ERROR)
-	{
-		wprintf(L"Shit no success!\n");
-	}
-	else
-		wprintf(L"Yeah working\n\n");
-
-	ZeroMemory(buffer, sizeof(buffer));
-	pwsaResults = (LPWSAQUERYSET)buffer;
-
-	pwsaResults->dwNameSpace = NS_BTH;
-	pwsaResults->dwSize = sizeof(WSAQUERYSET);
-	pwsaResults->lpBlob = NULL;
-
-	while (WSALookupServiceNext(hLookup, LUP_RETURN_ADDR| LUP_RETURN_NAME, &swSize, pwsaResults) == NO_ERROR)
-	{
-
-		wprintf(L"Device #:%d\n", nDevicesFound);
-		wprintf(L"Device name:%s\n", pwsaResults->lpszServiceInstanceName);
-		wprintf(L"Device connected: %d\n",		(pwsaResults->dwOutputFlags & BTHNS_RESULT_DEVICE_CONNECTED));
-		wprintf(L"Device remembered: %d\n",		(pwsaResults->dwOutputFlags & BTHNS_RESULT_DEVICE_REMEMBERED)>0);
-		wprintf(L"Device authenticated: %d\n",	(pwsaResults->dwOutputFlags & BTHNS_RESULT_DEVICE_AUTHENTICATED)>0);
-	
-		btSock = (SOCKADDR_BTH*)(&pwsaResults->lpcsaBuffer->RemoteAddr);
-		wprintf(L"Local Bluetooth device is %04x%08x, server channel = %d\n",
-			GET_NAP(btSock->btAddr), GET_SAP(btSock->btAddr), btSock->port);
-		nDevicesFound++;	
-
-	}
-	WSALookupServiceEnd(hLookup);
 }
 
 
