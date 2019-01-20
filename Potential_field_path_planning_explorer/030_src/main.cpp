59c6b9f95d125c833ef5f40f5f04285067980a07
NaPiZip <napizip@mail.com>
2019-01-19 23:57:30 -0500

    Added basic 3D plot example.

--- Potential_field_path_planning_explorer/030_src/main.cpp
+++ Potential_field_path_planning_explorer/030_src/main.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtDataVisualization>
+
+using namespace QtDataVisualization;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    Q3DSurface surface;
+    surface.setFlag(Qt::FramelessWindowHint);
+
+    QSurfaceDataArray *data = new QSurfaceDataArray;
+    QSurfaceDataRow *dataRow1 = new QSurfaceDataRow;
+    QSurfaceDataRow *dataRow2 = new QSurfaceDataRow;
+
+    *dataRow1 << QVector3D(0.0f, 0.1f, 0.5f) << QVector3D(1.0f, 0.5f, 0.5f);
+    *dataRow2 << QVector3D(0.0f, 1.8f, 1.0f) << QVector3D(1.0f, 2.0f, 1.f);
+
+    *data  << dataRow1 << dataRow2;
+
+    QSurface3DSeries *series = new QSurface3DSeries;
+    series->dataProxy()->resetArray(data);
+    surface.addSeries(series);
+    surface.show();
 
     return a.exec();
+
 }
