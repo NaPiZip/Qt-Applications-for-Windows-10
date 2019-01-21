e171640566d5fe5db3868474cc73c49c779baa64
NaPiZip <napizip@mail.com>
2019-01-21 18:42:49 -0500

    Example of Q3DSurf.

--- Potential_field_path_planning_explorer/030_src/main.cpp
+++ Potential_field_path_planning_explorer/030_src/main.cpp
@@ -1,9 +1,41 @@
 #include "mainwindow.h"
 #include <QtWidgets>
+#include <QtDataVisualization/Q3DSurface>
+#include <QtDataVisualization/Q3DBars>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
+
+using namespace QtDataVisualization;
 
 int main(int argc, char *argv[])
 {    
+    QApplication app(argc, argv);
+    Q3DSurface *graph = new Q3DSurface();
+    QWidget *container = QWidget::createWindowContainer(graph);
+
+    if(!graph->hasContext())
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Couldn't initialize the OpenGl context.");
+        msgBox.exec();
+        return -1;
+    }
+    QSize screenSize = graph->screen()->size();
+    container->setMinimumSize(QSize(static_cast<int>(screenSize.width()/1.6f),
+                                    static_cast<int>(screenSize.height()/ 1.6f)));
+    container->setMaximumSize(screenSize);
+    container->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+    container->setFocusPolicy(Qt::StrongFocus);
+
+    QWidget *window = new QWidget;
+    QHBoxLayout *hLayout = new QHBoxLayout(window);
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    hLayout->addWidget(container,1);
+    hLayout->addLayout(vLayout);
+    vLayout->setAlignment(Qt::AlignTop);
+
+    /*
     QApplication a(argc, argv);
     QWidget window;
 
@@ -25,5 +57,8 @@ int main(int argc, char *argv[])
     window.setWindowTitle(QCoreApplication::translate("windowlayout","Title"));
     window.show();
     return a.exec();
+    */
 
+    window->show();
+    return app.exec();
 }
